f8a0009ac174db7b272aac468a32953fb842cf86
Jared Rounsavall <jrounsav@asu.edu>
2019-04-01 17:48:29 -0700

    Making a pie out of slices

--- packages/pie/lib/pie.js
+++ packages/pie/lib/pie.js
@@ -1,7 +1,15 @@
 import React from "react";
+import slice from "slice";
 
 export default class pie extends React.Component {
   render() {
-    return <p>Pie!</p>;
+    return (
+      <div>
+        <slice />
+        <slice />
+        <slice />
+        <slice />
+      </div>
+    );
   }
 }
